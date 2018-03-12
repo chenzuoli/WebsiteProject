604a32839e8aa00782092afbeca54409ca97802e
chenzuoli <chenzuoli709@163.com>
2018-03-12 23:56:18 +0800

    将技术文章添加到项目中，并于登陆注册页面相结合

--- src/main/java/top/wetech/czl/controller/IndexController.java
+++ src/main/java/top/wetech/czl/controller/IndexController.java
@@ -13,7 +13,7 @@ import com.jfinal.core.Controller;
 public class IndexController extends Controller {
 
     public void index(){
-        render("index.jsp");
+        render("index.html");
     }
 
 }
